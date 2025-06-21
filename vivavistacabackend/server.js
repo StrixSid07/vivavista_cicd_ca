3ac21d89be1301557c37ba4c18091f28ebaa8dcd
RatiyaJay <116174128+RatiyaJay@users.noreply.github.com>
2025-06-21 12:17:26 +0530

    change express body incress

--- vivavistacabackend/server.js
+++ vivavistacabackend/server.js
@@ -115,6 +115,8 @@ app.use((req, res, next) => {
   req.user = { country: req.session.country }; // Attach selected country to req.user
   next();
 });
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 // ✅ API Routes
 app.use("/api/auth", require("./routes/authRoutes"));

9096340a1a37cee2ce3700edd48fa9e688515d03
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-06-24 02:02:12 +0530

    Update TermsAndConditions.jsx

--- vivavistaca/src/components/pages/TermsAndConditions.jsx
+++ vivavistaca/src/components/pages/TermsAndConditions.jsx
@@ -37,91 +37,38 @@ const TermsAndConditions = () => {
       </h1>
 
       <section className="mb-6">
-        <p className="mb-4">
-          Below are the Booking Conditions under which Viva Vista Vacations will
-          arrange bookings for flights, accommodations, transfers, excursions,
-          train journeys, cruises, ferries, motor coaches, or other holiday
-          requirements (the "Travel Arrangements"). The combination of travel
-          arrangements offered constitutes a package within the meaning of the
-          Package Travel and Linked Travel Arrangements Regulations. Therefore,
-          you will benefit from all EU rights applicable to packages. Viva Vista
-          Vacations is fully responsible for the proper performance of the
-          package as a whole.
+        <p className="mb-4 font-medium">
+          Below are the Booking Conditions under which Viva Vista Vacations will arrange bookings for flights, accommodations, transfers, excursions, train journeys, cruises, ferries, motor coaches, or other holiday requirements (the "Travel Arrangements"). The combination of travel arrangements offered constitutes a package within the meaning of the Package Travel and Linked Travel Arrangements Regulations. Therefore, you will benefit from all EU rights applicable to packages. Viva Vista Vacations is fully responsible for the proper performance of the package as a whole.
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-2xl font-semibold mb-4">Booking Conditions</h2>
         <p className="mb-4">
-          We are Viva Vista Vacations (hereinafter referred to as "Viva Vista",
-          "we", "us", or "our"), a registered travel agency incorporated in the
-          United Kingdom, trading as a Travel Organiser and Travel Agent
-          (company registration number 16227067). Our registered office address
-          is 01, 195-197 Wood Street, London, England, E17 3NU.
+          The following terms and conditions ("Terms and Conditions") apply to all travel and travel-related services or arrangements offered for sale by Vivavistavacations.ca. Vivavistavacations.ca is a trading name of Viva Vista Vacations Ltd. These booking conditions form the basis of your contract with us. The terms "service," "services," or "travel arrangements" used throughout refer to bookings for flights, accommodation or hotels, resorts, transfers, excursions, car rentals, trains, cruises, ferries, coaches, or any other travel-related services.
         </p>
-        <p>
-          References to "you" and "your" in these terms apply to the traveller
-          or purchaser of travel services and any other person in the party. By
-          confirming your booking, we assume that you have read and agreed to
-          these Booking Conditions. Unless otherwise stated, these conditions
-          apply only to holiday arrangements booked through us in the UK.
-          References to "holiday", "booking", "contract", "package", "tour", or
-          "arrangements" refer to travel services unless specified otherwise.
+        <p className="mb-4">
+          All references in these terms to "trip," "booking," "contract," "holiday," or "vacation" refer to such travel arrangements or any combination of services offered by us, unless otherwise specified.
         </p>
+        <p className="mb-4">
+          We are Viva Vista Vacations Ltd. References to "we," "us," "our," or Vivavistavacations refer to Viva Vista Vacations Ltd, a company registered in England and Wales with company number 16227067. Our registered office is located at 01 195-197 Wood Street, London,  E17 3NU, United Kingdom. Bookings made with us are subject to the conditions outlined below. Unless otherwise stated, these conditions apply solely to travel arrangements booked directly through us that we agree to provide or arrange under our agreement with you.
+        </p>
+        <p className="mb-4">
+          References to "you" or "your" refer to the individual booking the travel arrangements or all persons named in the booking (including those added or substituted later). It is your responsibility to read these conditions carefully, as they outline the respective rights and obligations of both parties. By asking us to confirm your booking, we assume that you have read, understood, and agreed to these booking conditions.
+        </p>
+        <p className="mb-4">
+          All bookings made through Vivavistavacations are also subject to the specific terms and conditions of the third-party suppliers providing the individual components of your travel arrangements.
+        </p>
+        <p className="mb-4">
+          In this document, a "package" refers to the pre-arranged combination of at least two of the following elements sold or offered for sale at an inclusive price, covering more than 24 hours or including overnight accommodation:
+        </p>
+        <ul className="list-disc pl-6 mb-4">
+          <li>(a) Transport</li>
+          <li>(b) Accommodation</li>
+          <li>(c) Other tourism-related services not ancillary to transport or accommodation and forming a significant part of the package.</li>
+        </ul>
       </section>
 
-      <section className="mb-6">
-        <h2 className="text-2xl font-semibold mb-4">
-          Our Role in Your Booking
-        </h2>
-        <p className="mb-4">
-          We act as a Travel Organiser, meaning that when you book a package
-          holiday with us and receive a single receipt for the total package
-          price, we are responsible for the full arrangement and operation of
-          the holiday. Once confirmed, your package will be considered a Single
-          Package Contract, and we will be responsible for the booking under
-          these Terms and Conditions.
-        </p>
-        <p>
-          When acting as a Travel Organiser, we arrange for you to enter into
-          contracts with principal suppliers, including airlines, accommodation
-          providers, tour operators, and cruise companies, among others. We are
-          the package organiser and assume the related responsibilities outlined
-          in these conditions. However, when you book multiple Travel
-          Arrangements in a way that forms a package holiday, we will be
-          responsible for that as a Multi-Contract Package in compliance with
-          the Package Travel and Linked Travel Arrangements Regulations 2018.
-        </p>
-      </section>
-
-      <section className="mb-6">
-        <h2 className="text-2xl font-semibold mb-4">Your Holiday Booking</h2>
-        <p className="mb-4">
-          A booking is confirmed as soon as we issue our confirmation invoice.
-          By making a booking with Viva Vista Vacations, you confirm that you
-          have the authority to accept and agree to these booking conditions on
-          behalf of your travel party.
-        </p>
-        <p>
-          When booking a package holiday, we will arrange for you to enter into
-          contracts with the principal(s) or other suppliers (such as tour
-          operators, airlines, accommodation providers, and other travel service
-          providers) listed on your confirmation. In these cases, Viva Vista
-          Vacations acts as a package organiser and takes responsibility for the
-          proper performance of your travel package, as outlined in these
-          booking conditions. As an agent, we do not accept responsibility for
-          the actions or omissions of the supplier(s) or for the travel services
-          provided by them unless your booking has been made in a way that forms
-          a package (see Clause 4 for further details). In such cases, Viva
-          Vista Vacations will accept responsibility as the package organiser.
-        </p>
-        <p className="mt-2">
-          Before booking, you will be informed of the organiser responsible for
-          your travel arrangements. This may be Viva Vista Vacations or a
-          third-party tour operator, in which case Viva Vista Vacations acts as
-          an agent on your behalf.
-        </p>
-      </section>
 
       <div className="max-w-4xl mx-auto p-6">
         {loading ? (
@@ -138,9 +85,8 @@ const TermsAndConditions = () => {
               >
                 <span>{term.title}</span>
                 <FaChevronDown
-                  className={`transition-transform duration-300 text-deep-orange-600 ${
-                    openIndex === index ? "rotate-180" : ""
-                  }`}
+                  className={`transition-transform duration-300 text-deep-orange-600 ${openIndex === index ? "rotate-180" : ""
+                    }`}
                 />
               </button>
 
